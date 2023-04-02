f779ff703ec43bba1e4e803aa7e38416e36c94c6
stefanagloginic <stefana.gloginic@gmail.com>
2023-04-02 00:20:53 -0700

    [marketplceLaunch] uncomment additional details

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,39 +1,24 @@
 import Link from "next/link";
 import { Fragment, useState } from "react";
-import { Badge, Box, Drawer /*, Button, styled, Dialog */ } from "@mui/material";
+import { Badge, Box, Drawer, styled } from "@mui/material";
 import Container from "@mui/material/Container";
-// import { useTheme } from "@mui/material/styles";
+import { useTheme } from "@mui/material/styles";
 import IconButton from "@mui/material/IconButton";
-// import useMediaQuery from "@mui/material/useMediaQuery";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import clsx from "clsx";
-// import Icon from "components/icons";
-// import { useAppContext } from "contexts/AppContext";
-// import MiniCart from "components/MiniCart";
+import MiniCart from "components/MiniCart";
 import Logo from "components/primitives/Logo";
-import { /* FlexBetween, */ FlexBox } from "components/flex-box";
+import { FlexBetween, FlexBox } from "components/flex-box";
 import ShoppingBagOutlined from "components/icons/ShoppingBagOutlined";
+// import { useAppContext } from "contexts/AppContext";
 
-// styled component
-// export const HeaderWrapper = styled(Box)(({
-//   theme
-// }) => ({
-//   zIndex: 3,
-//   width: "100%",
-//   height: ['64px', '80px', '80px'],
-//   transition: "height 250ms ease-in-out",
-//   background: 'transparent'
-// }));
-// const StyledContainer = styled(Container)({
-//   gap: 2,
-//   height: "100%",
-//   display: "flex",
-//   alignItems: "center",
-//   justifyContent: "space-between"
-// });
-
-// ==============================================================
-
-// ==============================================================
+const StyledContainer = styled(Container)({
+  gap: 2,
+  height: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between"
+});
 
 const Header = ({
   isAbsolute = false,
@@ -43,64 +28,63 @@ const Header = ({
   searchInput,
   rightChildren = []
 }) => {
-  // const theme = useTheme();
+  const theme = useTheme();
   // const {
   //   state
   // } = useAppContext();
   const [dialogOpen, setDialogOpen] = useState(false);
   const [sidenavOpen, setSidenavOpen] = useState(false);
-  // const downMd = useMediaQuery(theme.breakpoints.down(1150));
-  // const toggleDialog = () => setDialogOpen(!dialogOpen);
+  const downMd = useMediaQuery(theme.breakpoints.down(1150));
   const toggleSidenav = () => setSidenavOpen(!sidenavOpen);
 
-  // // FOR SMALLER DEVICE
-  // if (downMd) {
-  //   const ICON_STYLE = {
-  //     fontSize: 20
-  //   };
+  // FOR SMALLER DEVICE
+  if (downMd) {
+    const ICON_STYLE = {
+      fontSize: 20
+    };
 
-  //   return <Box className={clsx(className)} sx={{
-  //     zIndex: 3,
-  //     width: "100%",
-  //     height: ['64px', '80px', '80px'],
-  //     transition: "height 250ms ease-in-out",
-  //     background: 'transparent',
-  //     position: isAbsolute ? 'absolute' : 'relative'
-  //   }}>
-  //       <StyledContainer>
-  //         <FlexBetween width="100%">
-  //           {
-  //             isLogoShown ? (
-  //               <Link href="/">
-  //                 <Logo />
-  //               </Link>
-  //             ) : null
-  //           }
+    return <Box className={clsx(className)} sx={{
+      zIndex: 3,
+      width: "100%",
+      height: ['64px', '80px', '80px'],
+      transition: "height 250ms ease-in-out",
+      background: 'transparent',
+      position: isAbsolute ? 'absolute' : 'relative'
+    }}>
+        <StyledContainer>
+          <FlexBetween width="100%">
+            {
+              isLogoShown ? (
+                <Link href="/">
+                  <Logo />
+                </Link>
+              ) : null
+            }
 
-  //           {/* RIGHT CONTENT - LOGIN, CART, SEARCH BUTTON */}
-  //           <FlexBox justifyContent="end" flex={1}>
-  //             {
-  //               isCartShown ? (
-  //                 <>
-  //                 <Drawer open={sidenavOpen} anchor="right" onClose={toggleSidenav} sx={{
-  //                   zIndex: 9999
-  //                 }}>
-  //                   <MiniCart toggleSidenav={toggleSidenav} />
-  //                 </Drawer>
-  //                   <Box onClick={toggleSidenav} component={IconButton} sx={{'backgroundColor': '#f5f6f1'}}>
-  //                     <Badge badgeContent={state.cart.items.length} color="primary">
-  //                       <Icon.CartBag sx={ICON_STYLE} />
-  //                     </Badge>
-  //                   </Box>
-  //                 </>
-  //               ) : null
-  //             }
-  //           </FlexBox>
-  //         </FlexBetween>
-  //         { rightChildren }
-  //       </StyledContainer>
-  //     </Box>;
-  // }
+            {/* RIGHT CONTENT - LOGIN, CART, SEARCH BUTTON */}
+            <FlexBox justifyContent="end" flex={1}>
+              {
+                isCartShown ? (
+                  <>
+                  <Drawer open={sidenavOpen} anchor="right" onClose={toggleSidenav} sx={{
+                    zIndex: 9999
+                  }}>
+                    <MiniCart toggleSidenav={toggleSidenav} />
+                  </Drawer>
+                    <Box onClick={toggleSidenav} component={IconButton} sx={{'backgroundColor': '#f5f6f1'}}>
+                      <Badge badgeContent={state.cart.items.length} color="primary">
+                        <Icon.CartBag sx={ICON_STYLE} />
+                      </Badge>
+                    </Box>
+                  </>
+                ) : null
+              }
+            </FlexBox>
+          </FlexBetween>
+          { rightChildren }
+        </StyledContainer>
+      </Box>;
+  }
 
   return <Box className={clsx(className)} sx={{
       zIndex: 3,
@@ -127,7 +111,7 @@ const Header = ({
             </FlexBox>
           ) : null
         }
-        {/* LOGIN AND CART BUTTON */}
+        {/* CART BUTTON */}
         <FlexBox gap={1.5} alignItems="center">
           {
             isCartShown ? (
@@ -141,14 +125,14 @@ const Header = ({
           { rightChildren }
         </FlexBox>
 
-        {/* LOGIN FORM DIALOG AND CART SIDE BAR  */}
+        {/* CART SIDE BAR  */}
         <Fragment>
           {
             isCartShown ? (
               <Drawer open={sidenavOpen} anchor="right" onClose={toggleSidenav} sx={{
                 zIndex: 9999
               }}>
-                {/*<MiniCart toggleSidenav={toggleSidenav} />*/}
+                <MiniCart toggleSidenav={toggleSidenav} />
               </Drawer>
             ) : null
           }

69f303ecdc5a1d5f29eff68bc931091888ba187a
stefanagloginic <stefana.gloginic@gmail.com>
2023-04-02 01:08:49 -0700

    [marketplceLaunch] more cleanup and add minicart back in

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,22 +8,10 @@ import IconButton from "@mui/material/IconButton";
 import clsx from "clsx";
 // import Icon from "components/icons";
 // import { useAppContext } from "contexts/AppContext";
-// import MiniCart from "components/MiniCart";
+import MiniCart from "components/MiniCart";
 import Logo from "components/primitives/Logo";
 import { /* FlexBetween, */ FlexBox } from "components/flex-box";
 import ShoppingBagOutlined from "components/icons/ShoppingBagOutlined";
-
-// const StyledContainer = styled(Container)({
-//   gap: 2,
-//   height: "100%",
-//   display: "flex",
-//   alignItems: "center",
-//   justifyContent: "space-between"
-// });
-
-// ==============================================================
-
-// ==============================================================
 
 const Header = ({
   isAbsolute = false,
@@ -39,58 +27,7 @@ const Header = ({
   // } = useAppContext();
   const [dialogOpen, setDialogOpen] = useState(false);
   const [sidenavOpen, setSidenavOpen] = useState(false);
-  // const downMd = useMediaQuery(theme.breakpoints.down(1150));
-  // const toggleDialog = () => setDialogOpen(!dialogOpen);
   const toggleSidenav = () => setSidenavOpen(!sidenavOpen);
-
-  // // FOR SMALLER DEVICE
-  // if (downMd) {
-  //   const ICON_STYLE = {
-  //     fontSize: 20
-  //   };
-
-  //   return <Box className={clsx(className)} sx={{
-  //     zIndex: 3,
-  //     width: "100%",
-  //     height: ['64px', '80px', '80px'],
-  //     transition: "height 250ms ease-in-out",
-  //     background: 'transparent',
-  //     position: isAbsolute ? 'absolute' : 'relative'
-  //   }}>
-  //       <StyledContainer>
-  //         <FlexBetween width="100%">
-  //           {
-  //             isLogoShown ? (
-  //               <Link href="/">
-  //                 <Logo />
-  //               </Link>
-  //             ) : null
-  //           }
-
-  //           {/* RIGHT CONTENT - LOGIN, CART, SEARCH BUTTON */}
-  //           <FlexBox justifyContent="end" flex={1}>
-  //             {
-  //               isCartShown ? (
-  //                 <>
-  //                 <Drawer open={sidenavOpen} anchor="right" onClose={toggleSidenav} sx={{
-  //                   zIndex: 9999
-  //                 }}>
-  //                   <MiniCart toggleSidenav={toggleSidenav} />
-  //                 </Drawer>
-  //                   <Box onClick={toggleSidenav} component={IconButton} sx={{'backgroundColor': '#f5f6f1'}}>
-  //                     <Badge badgeContent={state.cart.items.length} color="primary">
-  //                       <Icon.CartBag sx={ICON_STYLE} />
-  //                     </Badge>
-  //                   </Box>
-  //                 </>
-  //               ) : null
-  //             }
-  //           </FlexBox>
-  //         </FlexBetween>
-  //         { rightChildren }
-  //       </StyledContainer>
-  //     </Box>;
-  // }
 
   return <Box className={clsx(className)} sx={{
       zIndex: 3,
@@ -138,7 +75,7 @@ const Header = ({
               <Drawer open={sidenavOpen} anchor="right" onClose={toggleSidenav} sx={{
                 zIndex: 9999
               }}>
-                {/*<MiniCart toggleSidenav={toggleSidenav} />*/}
+                <MiniCart toggleSidenav={toggleSidenav} />
               </Drawer>
             ) : null
           }
